1763c7933ea31ae6068c2aa3fee5910654d5a616
Guillermo Martín <martin.cagui25@salesianos.triana.edu>
2025-11-28 09:04:23 +0100

    Actualizacion de los ficheros de la calculadora

--- CalculadoraSimple/src/calculadora/OperacionSuma.java
+++ CalculadoraSimple/src/calculadora/OperacionSuma.java
@@ -1,7 +1,9 @@
 package calculadora;
+
 public class OperacionSuma {
 	
     public static int sumar(int a, int b) {
+    	System.out.println("Realizando la operación de suma...");
         return a + b;
     }
     
--- CalculadoraSimple/src/calculadora/Principal.java
+++ CalculadoraSimple/src/calculadora/Principal.java
@@ -4,6 +4,7 @@ public class Principal {
 	
     public static void main(String[] args) {
     	
+    	System.out.println("Bienvenido a la calculadora simple.");
         int resultado = OperacionSuma.sumar(5, 3);
         System.out.println("El resultado de la suma es: " + resultado);
     
