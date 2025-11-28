e32509c4d3eca7daa6056bda13b4400712c1df11
Guillermo Martín <martin.cagui25@salesianos.triana.edu>
2025-11-28 08:53:50 +0100

    Añadidos los ficheros de la calculadora

---
+++ CalculadoraSimple/src/calculadora/Principal.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class Principal {
+	
+    public static void main(String[] args) {
+    	
+        int resultado = OperacionSuma.sumar(5, 3);
+        System.out.println("El resultado de la suma es: " + resultado);
+    
+    }
+
+}
